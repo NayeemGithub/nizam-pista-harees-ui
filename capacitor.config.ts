be0672fcb412e3f6d34b0df055854a3e45b4ac8f
Nayeem <nayeemkhan555@gmail.com>
2025-03-09 10:12:13 +0530

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nizam-pista-harees-ionic',
+  webDir: 'www'
+};
+
+export default config;
